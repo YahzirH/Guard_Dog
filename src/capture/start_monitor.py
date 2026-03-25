20596eaa360cbbfbb3b5a39361768567b1ebccec
Yahzir Hood <yahyaboss1218@gmail.com>
2026-03-24 22:57:58 -0400

    relocating files because im not gonna make test rn and it looks ugly all sitting in src

--- src/capture/start_monitor.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def setup_monitor_mode(interface):
-    subprocess.run(['sudo', 'airmon-ng', interface, 'down'])
-    subprocess.run(['sudo', 'airmon-ng', 'start', interface])
-    print(f"{interface} enabled in monitor mode.")
-
-setup_monitor_mode("wlan0")
