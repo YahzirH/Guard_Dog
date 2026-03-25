94919aad23ea7feea89327a23847ae419e1c1935
Yahzir Hood <yahyaboss1218@gmail.com>
2026-03-25 16:11:55 -0400

    Upated server to close after client severs connection

--- websockets/confirm_reboot/server.py
+++ websockets/confirm_reboot/server.py
@@ -4,18 +4,26 @@ import websockets
 async def recieve_confirmation(ws):
     print(f"Connection from: {ws.remote_address}")
     try:
-        file_bytes = await ws.recv()
-        print(file_bytes)
+        #Confirmation
+        confirmation_message = await ws.recv()
+        print(confirmation_message)
         await ws.send("Recieved Confirmation Successfully!")
+
     except websockets.exceptions.ConnectionClosed:
         print(f"Connection closed by client: {ws.remote_address}")
+    
     finally:
         print(f"Handler for {ws.remote_address} finsihed")
+        print("Closing server...")
+        server.close()
+        print("Server closed.")
 
 async def main():
-    async with websockets.serve(recieve_confirmation, "0.0.0.0", 8000):
-        print("Server running on ws://0.0.0.0:8000")
-        await asyncio.Future()
+    global server
+    server = await websockets.serve(recieve_confirmation, "0.0.0.0", 8000)
+    print("Server running on ws://0.0.0.0:8000")
+
+    await server.wait_closed()
 
 if __name__ == "__main__":
     # Run the asynchronous server function
